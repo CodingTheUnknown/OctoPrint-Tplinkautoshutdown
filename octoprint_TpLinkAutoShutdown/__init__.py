2d8e3e8d6db59b5bf308b38dfb00ff00b6523045
James McCannon <jamesmccannon@James-Mac-mini.local>
2021-03-05 21:00:29 +0000

    Adding settings instructions into the settings page

--- octoprint_TpLinkAutoShutdown/__init__.py
+++ octoprint_TpLinkAutoShutdown/__init__.py
@@ -151,10 +151,10 @@ class TpLinkAutoShutdown(octoprint.plugin.StartupPlugin, octoprint.plugin.Settin
 			self._logger.info(data)
 			if data["type"] == "smartPlug":
 				self.conn = wallPlug(data["url"])
-				self.conn.update_three()
+				self.conn.update_two()
 			elif data["type"] == "smartStrip":
 				self.conn = wallStrip(data["url"])
-				self.conn.update_three()
+				self.conn.update_two()
 		except:
 			self._logger.warning("Error updating connection with the smart sockets within on_settings_save")
 		octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
